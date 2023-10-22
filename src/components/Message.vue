f90f54d1231e26091fc129dec3ae13179f95f502
Elias Dieter <EliDi@KOKE.GmbH.intern>
2023-10-22 15:11:11 +0200

    Adding a UI for the Desktio

---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container-message">
+        <div class="message">
+            <div>{{message.text}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Message',
+        props: [
+            'message'
+        ]
+    };
+</script>
